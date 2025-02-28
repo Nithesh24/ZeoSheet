2dfa0804775269a2d794c1ea578cd338a926d7d9
Nithesh24 <65641597+Nithesh24@users.noreply.github.com>
2025-03-01 00:15:30 +0530

    basic header layout added

--- zeo-sheet/src/componenets/header.js
+++ zeo-sheet/src/componenets/header.js
@@ -1,13 +1,32 @@
+import "../styleSheets/header.css";
 
+//images
+import logo from "../assets/zeoTapLogo.png";
 
 const Header = (props) =>{
     return(
-        <div className = "header-flex">
-            <h1>Tool 1</h1>
-            <h1>Tool 2</h1>
-            <h1>Tool 3</h1>
+        <div className = "main-flex">
+            <div className = "row-1">
+                <div className = "row-1-col-1">
+                    <img src = {logo} alt = {props.logoAlt} />
+                </div>
+                <div className = "row-1-col-2">
+                    <div className = "row-1-col-2-row-1">File Name</div>
+                    <div className = "row-1-col-2-row-2">
+                        <button className = "btn">Some Option</button>
+                    </div>
+                </div>
+            </div>
+            <div className = "row-2">
+                <button className = "btn">Some Option</button>
+                <button className = "btn">Some Option</button>
+            </div>
         </div>
     );
 }
 
+Header.defaultProps = {
+    logoSrc : logo,
+    logoAlt : "logo"
+}
 export default Header;
