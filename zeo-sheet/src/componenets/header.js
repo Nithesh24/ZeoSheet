ecdfdf2df84368c657319f8ae472b31c04324c74
Nithesh24 <65641597+Nithesh24@users.noreply.github.com>
2025-03-02 21:16:30 +0530

    css update

--- zeo-sheet/src/componenets/header.js
+++ zeo-sheet/src/componenets/header.js
@@ -3,22 +3,22 @@ import "../styleSheets/header.css";
 //images
 import logo from "../assets/logo.png";
 
-const Header = (props) =>{
-    return(
-        <div className = "main-flex">
-            <div className = "row-1">
-                <div className = "row-1-col-1">
-                    <img src = {logo} alt = {props.logoAlt} />
+const Header = (props) => {
+    const handleAboutClick = () => {
+        window.location.href = "https://github.com/Nithesh24/ZeoSheet";
+    };
+
+    return (
+        <div className="main-flex">
+            <div className="row-1">
+                <div className="row-1-col-1">
+                    <img src={logo} alt={props.logoAlt} />
                 </div>
-                <div className = "row-1-col-2">
-                    <div className = "row-1-col-2-row-1">Zeo Sheet</div>
-                    <div className = "row-1-col-2-row-2">
-                        <button className = "btn">File</button>
-                        <button className = "btn">Edit</button>
-                        <button className = "btn">View</button>
-                        <button className = "btn">Tools</button>
-                        <button className = "btn">Help</button>
-                        <button className = "btn">About</button>
+                <div className="row-1-col-2">
+                    <div className="row-1-col-2-row-1">Zeo Sheet</div>
+                    <div className="row-1-col-2-row-2">
+                        <p>Welcome to ZeoSheet, your personal Google Sheets alternative!</p>
+                        <button className="btn" onClick={handleAboutClick}>About</button>
                     </div>
                 </div>
             </div>
@@ -27,7 +27,7 @@ const Header = (props) =>{
 }
 
 Header.defaultProps = {
-    logoSrc : logo,
-    logoAlt : "logo"
+    logoSrc: logo,
+    logoAlt: "logo"
 }
 export default Header;
